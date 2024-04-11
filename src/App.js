01d1f1ba325ab4b018d11ca2a6b69bb9b12498af
rithinkt <rithinkt@gmail.com>
2024-04-11 13:37:40 +0530

    Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, {useEffect} from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import Contact from "./components/Pages/Contact";
 import Service from "./components/Pages/Service";
@@ -7,11 +7,29 @@ import Singleservice from "./components/Pages/Singleservice";
 import About from "./components/Pages/About";
 import Career from "./components/Pages/Career";
 import Slider from "./components/Pages/Slider";
+import ReactGA from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+    // Initialize Google Analytics with your tracking ID
+    ReactGA.initialize('G-PRKMHN91LW');
+  }, []);
+
+  // Track page views on route changes
+  const TrackPageViews = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+      ReactGA.pageview(location.pathname + location.search);
+    }, [location]);
+
+    return null;
+  };
   return (
     <div>
       <BrowserRouter>
+      <TrackPageViews/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Contact" element={<Contact />} />
